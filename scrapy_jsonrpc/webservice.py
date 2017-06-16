f10bc10c358d5b4dff88c036c142c8a08df82e74
candale <candale.andrei@gmail.com>
2017-06-17 00:21:02 +0300

    Python3 compatible WIP
    Compatible with 2.7

--- scrapy_jsonrpc/webservice.py
+++ scrapy_jsonrpc/webservice.py
@@ -1,4 +1,5 @@
 import logging
+import six
 
 from twisted.web import server, resource
 
@@ -17,27 +18,34 @@ logger = logging.getLogger(__name__)
 class JsonResource(JsonResource_):
 
     def __init__(self, crawler, target=None):
-        JsonResource_.__init__(self)
+        super(JsonResource, self).__init__()
+
         self.crawler = crawler
         self.json_encoder = ScrapyJSONEncoder(crawler=crawler)
+
+    def getChildWithDefault(self, path, request):
+        path = path.decode()
+        return super(JsonResource, self).getChildWithDefault(path, request)
+
 
 class JsonRpcResource(JsonResource):
 
     def __init__(self, crawler, target=None):
-        JsonResource.__init__(self, crawler, target)
+        super(JsonRpcResource, self).__init__(crawler, target)
+
         self.json_decoder = ScrapyJSONDecoder(crawler=crawler)
         self.crawler = crawler
         self._target = target
 
-    def render_GET(self, txrequest):
+    def render_GET(self, request):
         return self.get_target()
 
-    def render_POST(self, txrequest):
-        reqstr = txrequest.content.getvalue()
+    def render_POST(self, request):
+        reqstr = request.content.getvalue()
         target = self.get_target()
         return jsonrpc_server_call(target, reqstr, self.json_decoder)
 
-    def getChild(self, name, txrequest):
+    def getChild(self, name, request):
         target = self.get_target()
         try:
             newtarget = getattr(target, name)
@@ -54,33 +62,37 @@ class CrawlerResource(JsonRpcResource):
     ws_name = 'crawler'
 
     def __init__(self, crawler):
-        JsonRpcResource.__init__(self, crawler, crawler)
+        super(CrawlerResource, self).__init__(crawler, target=crawler)
 
 
 class RootResource(JsonResource):
 
-    def render_GET(self, txrequest):
-        return {'resources': self.children.keys()}
+    def render_GET(self, request):
+        return {'resources': list(map(six.u, self.children))}
 
-    def getChild(self, name, txrequest):
+    def getChild(self, name, request):
         if name == '':
             return self
-        return JsonResource.getChild(self, name, txrequest)
+        return JsonResource.getChild(self, name, request)
 
 
-class WebService(server.Site):
+class WebService(server.Site, object):
 
     def __init__(self, crawler):
         if not crawler.settings.getbool('JSONRPC_ENABLED'):
             raise NotConfigured
-        self.crawler = crawler
+
         logfile = crawler.settings['JSONRPC_LOGFILE']
+        self.crawler = crawler
         self.portrange = [int(x) for x in crawler.settings.getlist('JSONRPC_PORT', [6023, 6073])]
         self.host = crawler.settings.get('JSONRPC_HOST', '127.0.0.1')
+        self.noisy = False
+
         root = RootResource(crawler)
         root.putChild('crawler', CrawlerResource(self.crawler))
-        server.Site.__init__(self, root, logPath=logfile)
-        self.noisy = False
+
+        super(WebService, self).__init__(root, logPath=logfile)
+
         crawler.signals.connect(self.start_listening, signals.engine_started)
         crawler.signals.connect(self.stop_listening, signals.engine_stopped)
 
@@ -90,10 +102,10 @@ class WebService(server.Site):
 
     def start_listening(self):
         self.port = listen_tcp(self.portrange, self.host, self)
+
         logger.debug(
             'Web service listening on {host.host:s}:{host.port:d}'.format(
                 host=self.port.getHost()))
 
     def stop_listening(self):
         self.port.stopListening()
-
